cf1cf373919f1e4739420e92b39365aeda9c41d0
Brian Kracoff <bkracoff@gmail.com>
2013-04-01 17:07:26 -0400

    add Importer daemon running in background

--- moodmusic.py
+++ moodmusic.py
@@ -4,14 +4,19 @@
 
 from ui.cli import *
 from song.song import Song
+from input.Import import *
+import myconfig
 
 def run(songPath):
-    
-    myCLI = CLI()
-    mySong = Song.song_from_filepath(songPath)
-    myCLI.play_song(mySong)
 
-    
+    #Start CLI
+    application = CLI()
+    mySong = Song.song_from_filepath(songPath)
+    application.play_song(mySong)
+
+    #Start background thread importer
+    importer = Importer(MUSIC_LIBRARY_FILE_PATH, 100)
+    importer.startDaemon()
 
 if __name__ == '__main__':
     if sys.argv[1:] and sys.argv[1] not in ('-h', '--help'):
--- myconfig_sample.py
+++ myconfig_sample.py
@@ -2,4 +2,9 @@
 # Rename to myconfig.py and enter your api key below
 
 ECHO_NEST_API_KEY = "EchoNest API Key goes here"
-MUSIC_FOLDER = "/path/to/music/folder"
+
+#Enter iTunes library xml file or other path
+MUSIC_LIBRARY_FILE_PATH = "/Users/bkracoff/Music/iTunes/iTunes Music Library.xml"
+
+#Legacy importer
+MUSIC_FOLDER = "/Users/bkracoff/Music/iTunes/iTunes Media/Music"
