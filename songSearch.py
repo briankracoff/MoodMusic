e11766dcb888c957583e4b34c128bd125b42a4f3
Tom <weavert503@gmail.com>
2013-04-05 11:51:54 -0400

    updated songSearch module to allow user to step through results

--- songSearch.py
+++ songSearch.py
@@ -1,27 +1,37 @@
-## This function takes user input to search the default library for a specified
-### song
-## Returns the local filepath of the closest match found
-
-## This search is primitve:
-### It simply looks for the two strings you provided in the filepath, independent
-### of order and which one is actually the Artist or Song title
-
 import os
 from myconfig import *
 
 
 def song_search():
-    searchpath = None
-    
-    while searchpath == None:
-        print "Enter an Artist:",
-        searcha = raw_input()
-        print "Enter a song:",
-        searchs = raw_input()
 
-        for dirname, dirnames, filenames in os.walk(MUSIC_FOLDER):
-            for filename in filenames:
-                currpath = os.path.join(dirname, filename)
-                if currpath.find(searcha) != -1 and currpath.find(searchs) != -1:
-                    searchpath = currpath
-    return searchpath
+
+    # Note that these fields are not
+    print "Enter an Artist:",
+    searcha = raw_input()
+    print "Enter a song:",
+    searchs = raw_input()
+
+
+    for dirname, dirnames, filenames in os.walk(MUSIC_FOLDER):
+        for filename in filenames:
+            currpath = os.path.join(dirname, filename)
+            if currpath.find(searcha) != -1 and currpath.find(searchs) != -1:
+                print 'Found the filepath:', currpath
+                print 'Is this what you were looking for? (y or n, or g to give up)'
+                answer = raw_input()
+                if answer == 'y':
+                    return currpath
+                elif answer == 'g':
+                    return fail_recirc()
+    return fail_recirc()
+    
+        
+
+def fail_recirc():
+    print 'Song not found in library'
+    print 'Would you like to try again? (y or n)'
+    answer = raw_input()
+    if answer == 'y':
+        return song_search()
+    else:
+        return None
